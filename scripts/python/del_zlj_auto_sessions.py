a4b018785cb5427c933717f25777e525cc8fd572
wurfkreuz <wurfkreuz@mail.ru>
2024-06-27 14:05:44 +0300

    n

--- scripts/python/del_zlj_auto_sessions.py
+++ scripts/python/del_zlj_auto_sessions.py
@@ -1,6 +1,18 @@
 import subprocess
+import pwd
+import os
 
-result = subprocess.run(r"zellij list-sessions | awk '($1 ~ /-/) {print $1}' | sed -e 's/\x1b\[[0-9;]*m//g'",
+# # [Errno 25] Inappropriate ioctl for device occurs because os.getlogin() relies
+# # on a terminal device, which isn't available when the script is executed by
+# # systemd.
+# current_user = os.getlogin()
+# print(f"Script is being executed by user: {current_user}")
+
+# Get the current username using the effective user ID
+current_user = pwd.getpwuid(os.geteuid()).pw_name
+print(f"Script is being executed by user: {current_user}")
+
+result = subprocess.run(r"zellij list-sessions | awk '/EXITED/ && ($1 ~ /-/) {print $1}' | sed -e 's/\x1b\[[0-9;]*m//g'",
                         shell=True, capture_output=True, text=True)
 
 # Split the output into lines

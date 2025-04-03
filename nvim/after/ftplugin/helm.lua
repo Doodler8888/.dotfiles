24aa6ad3afa47e80e07e0d4afb4b30c91bb38dd0
wurfkreuz <wurfkreuz@mail.ru>
2025-04-03 13:41:42 +0300

    helm treesitter fix

--- nvim/after/ftplugin/helm.lua
+++ nvim/after/ftplugin/helm.lua
@@ -1,15 +1,7 @@
-vim.bo.shiftwidth = 2
-
--- i also want to change how indentation works.
---
--- spec:
---   selector:
---     matchLabels:
---       app.kubernetes.io/name: {{ .Release.Name }}-fluentd
---   template:
---     metadata:
---
--- for instance, when i press enter from the end of 'metadata:' line or iif i
--- press 'o' from the normal mode, then new line is indentated on the same level
--- as 'metadata:'. but if the last non white character of the line is a colon,
--- then the new line shoud be additionally indentated by on tab.
+vim.bo.shiftwidth = 2 -- Or your preferred YAML indent width
+vim.bo.expandtab = true
+-- Try inheriting yaml indent expression if available
+-- This command might fail if yaml indent isn't loaded yet,
+-- but worth trying or finding the correct indentexpr source.
+vim.cmd("runtime! indent/yaml.vim")
+vim.bo.indentexpr = vim.bo.indentexpr -- Re-assign yaml's indentexpr if loaded
